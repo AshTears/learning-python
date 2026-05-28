f9b97eca8e7e203c812c6fb253cc3cb1b5ad1a4b
Ashika Shallow <shalashika@outlook.com>
2026-05-27 21:46:08 -0400

    Updated program to include tax variables

--- exercise_3/exercise_3.py
+++ exercise_3/exercise_3.py
@@ -4,21 +4,42 @@
 #gross_inc = float(input("Enter your gross income from your W-2 for 2020: "))
 #num_dep = int(input("How many dependents are you claiming? "))
 
-gross_inc = 150000
-num_dep = 2
-
 # Calculate taxable income
-tax_income = gross_inc - 12200 - (2000 * num_dep)
+# tax_income = gross_inc - 12200 - (2000 * num_dep)
+tax_income = 50000
+
+# Declaring variables for the tax rate ranges
+max10 = 9875
+max12 = 40125
+max22 = 85525
+max24 = 163300
+max32 = 207350
+max35 = 518400
+
+tier10_tax = max10 * 0.1
+tier12_tax = tier10_tax + ((max12 - max10) * 0.12)
+tier22_tax = tier12_tax + ((max22 - max12) * 0.22)
+tier24_tax = tier22_tax + ((max24 - max22) * 0.24)
+tier32_tax = tier24_tax + ((max32 - max24) * 0.32)
+tier35_tax = tier32_tax + ((max35 - max32) * 0.35)
 
 # Calculate tax due
-if tax_income <= 9875:
+
+if tax_income <= max10:
     tax_due = tax_income * 0.1
-elif tax_income <= 40125:
-    tax_due = (9875 * 0.1) + ((tax_income - 9875) * 0.12)
-elif tax_income <= 85525:
-    tax_due = (9875 * 0.1) + ((40125 - 9875) * 0.12) + ((tax_income - 40125) * 0.22)
-elif tax_income <= 163300:
-    tax_due = (9875 * 0.1) + ((40125 - 9875) * 0.12) + ((85525 - 40125) * 0.22) + ((tax_income - 85525 * 0.24))
+elif tax_income <= max12:
+    tax_due = tier10_tax + ((tax_income - max10) * 0.12)
+elif tax_income <= max22:
+    tax_due = tier12_tax + ((tax_income - max12) * 0.22)
+elif tax_income <= max24:
+    tax_due = tier22_tax + ((tax_income - max22) * 0.24)
+elif tax_income <= max32:
+    tax_due = tier24_tax + ((tax_income - max24) * 0.32)
+elif tax_income <= max35:
+    tax_due = tier32_tax + ((tax_income - max32) * 0.35)
+elif tax_income > max35:
+    tax_due = tier35_tax + ((tax_income - max35) * 0.37)
+
 
 # Print the result
 print("Tax Due:", tax_due)

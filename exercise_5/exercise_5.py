5c1b617f72f2c60fca487677ab82265412e7e0d3
Ashika Shallow <shalashika@outlook.com>
2026-06-07 20:55:55 -0400

    the complete program

--- exercise_5/exercise_5.py
+++ exercise_5/exercise_5.py
@@ -1,24 +1,38 @@
 # Exercise 5: A Complete List Program
 
-first_list = ["pears", "books", 25, "Beyonce", 18]
+first_list = ["pears", "books", "25", "Beyonce", "18"]
 
 first_name = input("Enter your first name: ")
 last_name = input("Enter your last name: ")
 
-list_option = ""
 new_list = first_list.copy()
+new_items= []
 
-while(list_option != "quit"):
+keep_going = "yes"      # Sentinel value to prevent an infinite loop
+
+# Loop to get new values to add or remove from first_list
+while(keep_going != "quit"):
     user_value = input("Enter any value: ")
     list_option = input("add or remove? ")
-    if user_value in new_list:
-        if list_option.lower() == "add":
+    if list_option.lower() == "add":
+        if user_value in new_list:       
             print("That value already exists")
         else:
-            new_list.remove(user_value)
-    else:
-        if list_option.lower() == "remove":
-            print("That item cannot be removed")
-        else:
             new_list.append(user_value)
+            new_items.append(user_value)
+            print(new_list)
+    else:
+        if user_value in new_list:
+            new_items.append(user_value)
+            new_list.remove(user_value)
+            print(new_list)          
+        else:
+            print("That item cannot be removed")                    
+    keep_going = input("Do you want to continue? yes or quit: ")
+
+# Display results
+print("Original list:", first_list)
+print("Items added or removed from the list:",new_items)
+print("Updated list:", new_list)
+print(f"Have a good day {first_name} {last_name}")
 
